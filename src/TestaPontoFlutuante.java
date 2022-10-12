fd56c6630281a9d6c456bcd7e12a108367ba8d2b
Daniel Pinna <danielpinna@outlook.com.br>
2022-10-12 15:41:56 -0300

    Add files examples

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario = 1250.70;
+		System.out.println(salario);
+		
+		//Divisao de inteiros, truncam o resultado para inteiro, mesmo quando o resultado tem ponto flutuante
+		int divisao = 5 / 2;
+		System.out.println(divisao);
+		
+		double outraDivisao = 5.0 / 2;
+		System.out.println(outraDivisao);
+	}
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,11 @@
+public class TestaVariaveis{
+
+    public static void main(String [] args){
+
+        System.out.println("Ola novo teste!!!");    
+
+        int idade = 38;
+
+        System.out.println("Sua idade e "+ idade + ", meus parabens!");
+    }
+}
